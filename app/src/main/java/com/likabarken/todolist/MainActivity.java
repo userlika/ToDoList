6ff2e7ca89fe7965836b353b6d8d3cd672053d66
userlika <likaforstudying@gmail.com>
2025-03-09 17:23:42 +0300

    Stepik-7.9: Remove elements with swipe

--- app/src/main/java/com/likabarken/todolist/MainActivity.java
+++ app/src/main/java/com/likabarken/todolist/MainActivity.java
@@ -6,11 +6,13 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -42,11 +44,35 @@ public class MainActivity extends AppCompatActivity {
         notesAdapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
             @Override
             public void onNoteClick(Note note) {
+
+            }
+        });
+        recycleViewNotes.setAdapter(notesAdapter);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(
+                new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(
+                    @NonNull RecyclerView recyclerView,
+                    @NonNull RecyclerView.ViewHolder viewHolder,
+                    @NonNull RecyclerView.ViewHolder target)
+            {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(
+                    @NonNull RecyclerView.ViewHolder viewHolder,
+                    int direction)
+            {
+                int position = viewHolder.getAdapterPosition();
+                Note note = notesAdapter.getNotes().get(position);
                 database.remove(note.getId());
                 showNotes();
             }
         });
-        recycleViewNotes.setAdapter(notesAdapter);
+
+        itemTouchHelper.attachToRecyclerView(recycleViewNotes);
 
         buttonAddNote.setOnClickListener(new View.OnClickListener() {
             @Override
