d88b4960b0f000e3e0ea291533b1a8932bff69ca
userlika <likaforstudying@gmail.com>
2025-03-09 20:49:01 +0300

    Stepik-7.10 Room database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
 }
 
 dependencies {
@@ -37,6 +38,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation(libs.room)
+    annotationProcessor(libs.room.annotation.processor)
+    implementation(libs.room.rxjava3)
+    implementation(libs.rxandroid)
+    implementation(libs.rxjava)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/likabarken/todolist/NoteDatabase.java
@@ -0,0 +1,27 @@
+package com.likabarken.todolist;
+
+import android.app.Application;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+// Номер версии необходимо поднимать, если произошли какие-то изменения в базе
+// Нр, добавилась новая таблица, изменилось поле в существующей таблице
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private static NoteDatabase instance = null;
+    private static final String DB_NAME = "notes.db";
+
+    public static NoteDatabase getInstance(Application application){
+        if (instance == null) {
+            instance = Room.databaseBuilder(
+                    application,
+                    NoteDatabase.class,
+                    DB_NAME
+            ).build();
+        }
+        return instance;
+    }
+}
