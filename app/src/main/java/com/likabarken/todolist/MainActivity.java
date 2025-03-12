41155723861e1ae18222bd45663fa06ea0e137a6
userlika <likaforstudying@gmail.com>
2025-03-12 23:27:07 +0300

    Stepik-7.15: LiveData

--- app/src/main/java/com/likabarken/todolist/MainActivity.java
+++ app/src/main/java/com/likabarken/todolist/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -31,9 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
     private NoteDatabase noteDatabase;
 
-    // Ссылка на главный поток нужна потому, что работать с view можно только из главного потока
-    // Держит ссылку на главный поток
-    private Handler handler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,46 +57,43 @@ public class MainActivity extends AppCompatActivity {
         });
         recycleViewNotes.setAdapter(notesAdapter);
 
-        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(
-                new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+        // Подписка на все изменения, которые произойдут в базе данных.
+        // При наличии изменений, новые данные прилетят в метод onChanged.
+        noteDatabase.notesDao().getNotes().observe(this, new Observer<List<Note>>() {
             @Override
-            public boolean onMove(
-                    @NonNull RecyclerView recyclerView,
-                    @NonNull RecyclerView.ViewHolder viewHolder,
-                    @NonNull RecyclerView.ViewHolder target)
-            {
-                return false;
-            }
-
-            @Override
-            public void onSwiped(
-                    @NonNull RecyclerView.ViewHolder viewHolder,
-                    int direction)
-            {
-                int position = viewHolder.getAdapterPosition();
-                Note note = notesAdapter.getNotes().get(position);
-
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // Создан поток, внутри которого удаляется элемент
-                        noteDatabase.notesDao().remove(note.getId());
-                        /*
-                         showNotes() нужно вызвать на главном потоке, поэтому отправляем хэндлеру
-                         сообщение в виде объекта Runnable, теперь хэндлер, который хранит ссылку на
-                         главный поток, будет обрабатывать все входящие сообщения
-                        */
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                showNotes();
-                            }
-                        });
-                    }
-                });
-                thread.start();
+            public void onChanged(List<Note> notes) {
+                notesAdapter.setNotes(notes);
             }
         });
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(
+                new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+                    @Override
+                    public boolean onMove(
+                            @NonNull RecyclerView recyclerView,
+                            @NonNull RecyclerView.ViewHolder viewHolder,
+                            @NonNull RecyclerView.ViewHolder target) {
+                        return false;
+                    }
+
+                    @Override
+                    public void onSwiped(
+                            @NonNull RecyclerView.ViewHolder viewHolder,
+                            int direction) {
+                        int position = viewHolder.getAdapterPosition();
+                        Note note = notesAdapter.getNotes().get(position);
+
+                        Thread thread = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Создан поток, внутри которого удаляется элемент
+                                noteDatabase.notesDao().remove(note.getId());
+
+                            }
+                        });
+                        thread.start();
+                    }
+                });
 
         itemTouchHelper.attachToRecyclerView(recycleViewNotes);
 
@@ -111,37 +106,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        showNotes();
-    }
 
     private void initViews() {
         recycleViewNotes = findViewById(R.id.recycleViewNotes);
         buttonAddNote = findViewById(R.id.buttonAddNote);
-    }
-
-    private void showNotes() {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // notesAdapter.setNotes(noteDatabase.notesDao().getNotes()); - тут получаем данные
-                // из базы, затем устанавливаем их в адаптер
-
-                /*
-                Рефакторинг: получение данных сделать в фоновом потоке, а установку данных сделать
-                в главном потоке.
-                 */
-                List<Note> notes = noteDatabase.notesDao().getNotes();
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        notesAdapter.setNotes(notes);
-                    }
-                });
-            }
-        });
-        thread.start();
     }
 }
